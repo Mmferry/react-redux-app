0844d21f896c22df0f7044bc10474ad34a5f22da
mferry <mfaired@gmail.com>
2019-10-31 00:21:25 +0200

    babel ND eslint config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,8 +24,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      favicon: "src/favicon.ico"
+      template: "src/index.html"
+      //favicon: "src/favicon.ico"
     })
   ],
   module: {
